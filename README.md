65a23bfe7c57003bee94fcab0f374683da9682b4
Cameron Kelley <cameron@kelley.vc>
2022-12-13 10:23:25 -0600

    add segmentation

--- README.md
+++ README.md
@@ -6,5 +6,13 @@
 nvidia-smi
 htop
 docker exec sd cat predict.log
-docker run --publish 60:5000 --name sd cog-speaker-diarization
+docker run --publish 80:5000 --name sd cog-speaker-diarization
+docker builder prune
+
+curl -X POST -H "Content-Type: application/json" -d '{"input": {"audio": "https://github.com/cameronk/cog-pyannote/blob/diarize/speaker-diarization/examples/jre-kevin-hart-youtube-short-vzx6h2sAGTU.wav?raw=true", "auth_token": "TOKEN_HERE"}}' http://127.0.0.1/predictions
 ```
+
+## VM setup
+
+1. https://docs.docker.com/desktop/install/debian/
+2. https://replicate.com/docs/guides/push-a-model#install-cog
---
+++ segmentation/predict.py
@@ -0,0 +1,88 @@
+from typing import Any, List 
+
+import numpy as np
+from cog import BasePredictor, Input, Path, BaseModel
+from pyannote.audio import Model
+from pyannote.audio.pipelines import VoiceActivityDetection, OverlappedSpeechDetection
+import torch
+import logging
+import ffmpeg
+
+class AnnotationJson(BaseModel):
+    pyannote: str
+    uri: str
+    modality: str
+    content: List[dict]
+
+AVAILABLE_TASKS = ["vad", "osd"]
+
+class Predictor(BasePredictor):
+    def setup(self):
+        """Load the model into memory to make running multiple predictions efficient"""
+        
+        # Check device
+        device_count = torch.cuda.device_count()
+        if device_count == 0: 
+            logging.warn("GPU unavailable!")
+
+        # Load model
+        self.model = Model.from_pretrained("config.yaml")
+        logging.info("Completed setup")
+        pass
+      
+    def predict(
+        self,
+        audio: Path = Input(description="Audio to perform voice activity detection on. Accepts any audio file type convertible to .wav by ffmpeg."),
+        task: str = Input(description="Segmentation task to perform", default="vad"),
+        onset: float = Input(description="Onset activation threshold", default=0.5),
+        offset: float = Input(description="Offset activation threshold", default=0.5),
+        min_duration_on: float = Input(description="Remove speech regions shorter than that many seconds.", default=0.0),
+        min_duration_off: float = Input(description="Fill non-speech regions shorter than that many seconds.", default=0.0)
+    ) -> AnnotationJson:
+        if task not in AVAILABLE_TASKS:
+            raise Exception("Task %s is not available. Available tasks are: %s" % (task, AVAILABLE_TASKS))
+
+        # Setup hyperparameters
+        HYPER_PARAMETERS = {
+            "onset": onset,
+            "offset": offset,
+            "min_duration_on": min_duration_on,
+            "min_duration_off": min_duration_off
+        }
+
+        # Setup pipeline
+        if task == "vad":
+            pipeline = VoiceActivityDetection(segmentation=self.model)
+        elif task == "osd":
+            pipeline = OverlappedSpeechDetection(segmentation=self.model)
+        
+        pipeline.instantiate(HYPER_PARAMETERS)
+
+        # Convert to wav if necessary
+        audio_path = audio
+        if audio.suffix != ".wav":
+            logging.info("Converting audio to wav")
+            audio_path = audio.with_suffix(".wav")
+            try: 
+                ffmpeg.input(str(audio)).output(str(audio_path)).run()
+            except Exception as e:
+                logging.exception(e)
+                raise e
+        
+        # Run pipeline
+        output = pipeline(audio_path)
+        logging.info("Task %s complete" % task)
+
+        data = output.for_json()
+        data.update({ "task": task, "parameters": HYPER_PARAMETERS })
+
+        # https://github.com/pyannote/pyannote-core/blob/develop/pyannote/core/annotation.py#L1502
+        # {
+        #   pyannote: "",
+        #   uri: "",
+        #   modality: "",
+        #   content: [{ segment: { start: 0, end: 1 }, track: "", label: "" }]
+        # }
+        return data
+
+
